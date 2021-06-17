a23019bbbf2ef500f2335e3e60b1359e31b473bb
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-17 18:16:11 +0300

    add url for Register page

--- rest_project/userprofile/urls.py
+++ rest_project/userprofile/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('profile/',ProfileView.as_view())
+    path('profile/',ProfileView.as_view(),name='profile'),
+    path('register/',RegisterView.as_view(),name='register'),
+
 ]
