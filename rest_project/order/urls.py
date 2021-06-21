f2d55bd435ba81ad53e414da3e7aef9dcf587e24
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-21 18:18:45 +0300

    create url for: my_orders,modify _order

--- rest_project/order/urls.py
+++ rest_project/order/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('order/',OrderPostView.as_view(),name='order'),
+    path('my_orders/',MyOrderView.as_view()),
+    path('modify_order/<int:order_id>/',UpdateDeleteView.as_view(),name='modify_order'),
 
 ]
