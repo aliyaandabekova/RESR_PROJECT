9aeea183ea7ccaa7120aed949f38154b89784574
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-21 18:17:22 +0300

    add related_name for profile in class Order

--- rest_project/order/models.py
+++ rest_project/order/models.py
@@ -4,7 +4,7 @@ from product.models import Product
 
 class Order(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
-    profile = models.ForeignKey(Profile,on_delete=models.SET_NULL,null=True)
+    profile = models.ForeignKey(Profile,on_delete=models.SET_NULL,null=True,related_name='orders')
     quantity = models.PositiveIntegerField(default=0)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(choices=(
