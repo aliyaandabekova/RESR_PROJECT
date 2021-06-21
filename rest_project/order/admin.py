ba6cc594c9dad25e4466fe2db849820b3ae2ae98
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-21 15:22:22 +0300

    add payment_method to list_display

--- rest_project/order/admin.py
+++ rest_project/order/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Order
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['profile','product','status','quantity','date_created','total_sum']
+    list_display = ['profile','product','status','quantity','date_created','total_sum','payment_method']
     readonly_fields = ['profile','date_created','total_sum']
 admin.site.register(Order,OrderAdmin)
