1c524ba1b80003921139c859b9b4e94f1ae91b4b
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-21 15:33:40 +0300

    delete profile,product

--- rest_project/order/views.py
+++ rest_project/order/views.py
@@ -3,13 +3,14 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from product.models import Product
 from .serializers import OrderSerializer
+from .services import *
 
 class OrderPostView(APIView):
     def post(self,request):
         serializer = OrderSerializer(data=request.data)
-        # profile = request.user.profile
-        # product = Product.objects.get()
+
         if serializer.is_valid():
             serializer.save()
+
             return Response(serializer.data,status=201)
         return Response(serializer.errors,status=400)
